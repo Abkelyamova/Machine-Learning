cef649f80414471833ba967378bcd85cb044e96d
Orlova-Tatiana <tanya.orlova.1997@mail.ru>
2017-10-09 17:39:24 +0300

    LOO для KNN

---
+++ oRRRlova/KNN.R
@@ -0,0 +1,80 @@
+#вспомогательные функции
+mc.euclideanDistance = function(p1, p2) sqrt(sum((p1 - p2) ^ 2))
+mc.getDistances = function(points, u, distFunction) apply(points, 1, distFunction, u)
+
+plot.limits = function(arr, deviation = 0) {
+    c(min(arr) - deviation, max(arr) + deviation)
+}
+
+#KNN
+mc.KNN = function(sortedDistances, k) {
+    minKDistances = sortedDistances[1:k]
+    minKClasses = names(minKDistances)
+    bestClass = names(which.max(table(minKClasses)))
+}
+
+#LOO
+mc.LOO.KNN = function(points, classes) {
+    n = dim(points)[1]
+    looY = rep(0, n-1) #n-1, потому что один элемент всегда будет отсутствовать из выборки
+
+    for (i in 1:n) {
+        u = points[i,]
+        teachSample = points[-i,]
+
+        distances = mc.getDistances(points, u, mc.euclideanDistance)[-i]
+        names(distances) = classes[-i]
+        sortedDistances = sort(distances)
+
+        for (k in 1:n-1) {
+            bestClass = mc.KNN(sortedDistances, k)
+            looY[k] = looY[k] + ifelse(bestClass == classes[i], 0, 1)
+        }
+    }
+
+    looY = looY / n
+}
+
+#DRAWINGS
+mc.draw.LOO.KNN = function(points, classes) {
+    looY = mc.LOO.KNN(points, classes)
+
+    plot(1:length(looY), looY, type = "l", main = "LOO для KNN", xlab = "K", ylab = "LOO")
+    k.opt = which.min(looY)
+    points(k.opt, looY[k.opt], pch = 19, col = "red")
+    text(k.opt, looY[k.opt], labels = paste("K=", k.opt, sep = ""), pos = 3, col = "red", family = "mono")
+
+    return(k.opt)
+}
+
+mc.draw.KNN = function(points, classes, colors, k, xlim, ylim, step) {
+    uniqueClasses = unique(classes)
+    names(colors) = uniqueClasses
+    plot(points, bg = colors[classes], pch = 21, asp = 1, xlim = xlim, ylim = ylim, main = "Карта классификации по KNN") #known data
+    #legend("topright", legend = uniqueClasses, pch = 20, pt.bg = colors, xpd = T)
+
+    #Классифицируем точки
+    for (x in seq(xlim[1], xlim[2], step)) {
+        for (y in seq(ylim[1], ylim[2], step)) {
+            u = c(round(x, 1), round(y, 1)) #избегаем случаев 0.1 + 0.2 = 0.3000000004
+            if (any(apply(points, 1, function(v) all(v == u)))) next #не классифицировать известные точки
+
+            distances = mc.getDistances(points, u, mc.euclideanDistance)
+            names(distances) = classes
+            bestClass = mc.KNN(sort(distances), k)
+
+            #рисуем новую классифицированную точку
+            points(u[1], u[2], col = colors[bestClass], pch = 21) #u
+        }
+    }
+}
+
+#тестируем программу
+main = function() {
+    petals = iris[, 3:4]
+    petalNames = iris[, 5]
+
+    par(mfrow = c(1, 2))
+    k.opt = mc.draw.LOO.KNN(petals, petalNames)
+    mc.draw.KNN(petals, petalNames, colors = c("red", "green3", "blue"), k = k.opt, xlim = plot.limits(petals[, 1], 0.2), ylim = plot.limits(petals[, 2], 0.2), step = 0.1)
+}
