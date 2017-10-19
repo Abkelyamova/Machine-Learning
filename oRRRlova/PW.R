814861b04839d4fe6ec6ad138d74a92b0e53cc18
Orlova-Tatiana <tanya.orlova.1997@mail.ru>
2017-10-19 22:06:41 +0300

    Парзеновское окно (PW)

---
+++ oRRRlova/PW.R
@@ -0,0 +1,113 @@
+#вспомогательные функции
+mc.dist = function(p1, p2) sqrt(sum((p1 - p2) ^ 2)) #Евклидово расстояние
+mc.distances = function(points, u) apply(points, 1, mc.dist, u) #Расстояние от всех points до точки u
+mc.sumByClass = function(class, arr) sum(arr[names(arr) == class]) #Суммирует значения каждого класса
+mc.contains = function(points, u) any(apply(points, 1, function(v) all(v == u)))
+
+plot.limits = function(arr, deviation = 0) c(min(arr) - deviation, max(arr) + deviation) #минимальное и максимальное значения с отклонением
+
+#Ядра
+mc.kernel.R = function(r) 0.5 * (abs(r) <= 1) #прямоугольное
+mc.kernel.T = function(r) (1 - abs(r)) * (abs(r) <= 1) #треугольное
+mc.kernel.Q = function(r) (15 / 16) * (1 - r ^ 2) ^ 2 * (abs(r) <= 1) #квартическое
+mc.kernel.E = function(r)(3 / 4) * (1 - r ^ 2) * (abs(r) <= 1) #епанечниково
+
+mc.PW.kernel = mc.kernel.R #использовать ЭТО ядро
+
+#PW
+mc.PW = function(distances, u, h) {
+    weights = mc.PW.kernel(distances / h)
+    classes = unique(names(distances))
+
+    weightsByClass = sapply(classes, mc.sumByClass, weights)
+
+    if (max(weightsByClass) == 0) return("") #ни одна точка не попала в окно
+
+    return(names(which.max(weightsByClass)))
+}
+
+#LOO
+mc.LOO.PW = function(points, classes, hValues) {
+    n = dim(points)[1]
+    loo = rep(0, length(hValues))
+
+    for (i in 1:n) {
+        u = points[i,]
+        sample = points[-i,]
+        distances = mc.distances(sample, u)
+        names(distances) = classes[-i]
+
+        for (j in 1:length(hValues)) {
+            h = hValues[j]
+            classified = mc.PW(distances, u, h)
+            loo[j] = loo[j] + (classified != classes[i])
+        }
+    }
+
+    loo = loo / n
+}
+
+#Отрисовка LOO
+mc.draw.LOO.PW = function(points, classes, hValues) {
+    loo = mc.LOO.PW(points, classes, hValues)
+
+    x = hValues
+    y = loo
+
+    plot(x, y, type = "l", main = "LOO для Парзеновского окна (PW)", xlab = "h", ylab = "LOO", col.lab = "blue")
+
+    h = hValues[which.min(loo)]
+    h.loo = round(loo[which.min(loo)], 4)
+
+    points(h, h.loo, pch = 19, col = "blue")
+    label = paste("h = ", h, "\n", "LOO = ", h.loo, sep = "")
+    text(h, h.loo, labels = label, pos = 3, col = "blue", family = "mono", font = 2)
+
+    return(h)
+}
+
+#Отрисовка карты классификации
+mc.draw.PW = function(points, classes, colors, h) {
+    uniqueClasses = unique(classes)
+    names(colors) = uniqueClasses
+
+    x = points[, 1]
+    y = points[, 2]
+    xlim = plot.limits(x, 0.3)
+    ylim = plot.limits(y, 0.3)
+    plot(points, bg = colors[classes], pch = 21, asp = 1, xlim = xlim, ylim = ylim, main = "Карта классификации PW", col.lab = "blue") #Рисуем известные точки
+
+    #Классифицируем точки
+    step = 0.1
+    ox = seq(xlim[1], xlim[2], step)
+    oy = seq(ylim[1], ylim[2], step)
+
+    for (x in ox) {
+        for (y in oy) {
+            x = round(x, 1) #избегаем случаев 0.1 + 0.2 = 0.3000000004
+            y = round(y, 1) #избегаем случаев 0.1 + 0.2 = 0.3000000004
+            u = c(x, y)
+
+            if (mc.contains(points, u)) next #не классифицировать известные точки
+
+            distances = mc.distances(points, u)
+            names(distances) = classes
+            classified = mc.PW(distances, u, h)
+
+            #рисуем новую классифицированную точку
+            points(u[1], u[2], col = colors[classified], pch = 21) #u
+        }
+    }
+
+    legend("topright", legend = uniqueClasses, pch = 21, pt.bg = colors[uniqueClasses], xpd = T) #добавим легенду для большей ясности
+}
+
+#тестируем программу
+test = function() {
+    petals = iris[, 3:4]
+    petalNames = iris[, 5]
+
+    par(mfrow = c(1, 2))
+    h = mc.draw.LOO.PW(petals, petalNames, hValues = seq(0.1, 2, 0.005))
+    mc.draw.PW(petals, petalNames, colors = c("red", "green3", "blue"), h = h)
+}
