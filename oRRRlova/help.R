f1e8809e80e8762e3056c5bdedd303f0a491e47f
Tatiana Orlova <tanya.orlova.1997@mail.ru>
2017-11-01 09:53:05 +0300

    utf-8

--- oRRRlova/help.R
+++ oRRRlova/help.R
@@ -1,12 +1,12 @@
-mc.dist = function(p1, p2) sqrt(sum((p1 - p2) ^ 2)) #Евклидово расстояние
-mc.distances = function(points, u) apply(points, 1, mc.dist, u) #Расстояние от всех points до точки u
-mc.sumByClass = function(class, arr) sum(arr[names(arr) == class]) #Суммирует значения каждого класса
+п»їmc.dist = function(p1, p2) sqrt(sum((p1 - p2) ^ 2)) #Р•РІРєР»РёРґРѕРІРѕ СЂР°СЃСЃС‚РѕСЏРЅРёРµ
+mc.distances = function(points, u) apply(points, 1, mc.dist, u) #Р Р°СЃСЃС‚РѕСЏРЅРёРµ РѕС‚ РІСЃРµС… points РґРѕ С‚РѕС‡РєРё u
+mc.sumByClass = function(class, arr) sum(arr[names(arr) == class]) #РЎСѓРјРјРёСЂСѓРµС‚ Р·РЅР°С‡РµРЅРёСЏ РєР°Р¶РґРѕРіРѕ РєР»Р°СЃСЃР°
 mc.contains = function(points, u) any(apply(points, 1, function(v) all(v == u)))
 
-plot.limits = function(arr, deviation = 0) c(min(arr) - deviation, max(arr) + deviation) #минимальное и максимальное значения с отклонением
+plot.limits = function(arr, deviation = 0) c(min(arr) - deviation, max(arr) + deviation) #РјРёРЅРёРјР°Р»СЊРЅРѕРµ Рё РјР°РєСЃРёРјР°Р»СЊРЅРѕРµ Р·РЅР°С‡РµРЅРёСЏ СЃ РѕС‚РєР»РѕРЅРµРЅРёРµРј
 
-#Ядра
-mc.kernel.R = function(r) 0.5 * (abs(r) <= 1) #прямоугольное
-mc.kernel.T = function(r)(1 - abs(r)) * (abs(r) <= 1) #треугольное
-mc.kernel.Q = function(r)(15 / 16) * (1 - r ^ 2) ^ 2 * (abs(r) <= 1) #квартическое
-mc.kernel.E = function(r)(3 / 4) * (1 - r ^ 2) * (abs(r) <= 1) #епанечниково
+#РЇРґСЂР°
+mc.kernel.R = function(r) 0.5 * (abs(r) <= 1) #РїСЂСЏРјРѕСѓРіРѕР»СЊРЅРѕРµ
+mc.kernel.T = function(r)(1 - abs(r)) * (abs(r) <= 1) #С‚СЂРµСѓРіРѕР»СЊРЅРѕРµ
+mc.kernel.Q = function(r)(15 / 16) * (1 - r ^ 2) ^ 2 * (abs(r) <= 1) #РєРІР°СЂС‚РёС‡РµСЃРєРѕРµ
+mc.kernel.E = function(r)(3 / 4) * (1 - r ^ 2) * (abs(r) <= 1) #РµРїР°РЅРµС‡РЅРёРєРѕРІРѕ
