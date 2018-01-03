9882d9820ea1de57f32e319d6fb90e15ea0e79fc
Orlova-Tatiana <tanya.orlova.1996@mail.ru>
2018-01-04 02:07:32 +0300

    Линии уровня

---
+++ oRRRlova/Level/server.R
@@ -0,0 +1,42 @@
+norma = function(x, y, mu, sigma) {
+    x = matrix(c(x, y), 1, 2)
+
+    k = 1 / sqrt((2 * pi) ^ 2 * det(sigma))
+    e = exp(-0.5 * (x - mu) %*% solve(sigma) %*% t(x - mu))
+    k * e
+}
+
+server = function(input, output) {
+    output$plot = renderPlot({
+        mu = matrix(c(0, 0), 1, 2)
+        sigma = matrix(c(input$cov11, input$cov12, input$cov21, input$cov22), 2, 2)
+
+        if (det(sigma) <= 0) {
+            output$covMessage = renderText({
+                "Определитель < 0"
+            })
+            return()
+        }
+        output$covMessage = renderText({
+            ""
+        })
+
+        # Рисуем дискриминантую функцию
+        minX = -sigma[1, 1] - 2
+        maxX = sigma[1, 1] + 2
+        minY = -sigma[2, 2] - 2
+        maxY = sigma[2, 2] + 2
+
+        x = seq(minX, maxX, len = 100)
+        y = seq(minY, maxY, len = 100)
+        z = outer(x, y, function(x, y) {
+            sapply(1:length(x), function(i) norma(x[i], y[i], mu, sigma))
+        })
+        
+        add = F
+        for (level in seq(from = input$by, to = 0.2, by = input$by)) {
+            contour(x, y, z, levels = level, drawlabels = T, lwd = 1, col = "black", add = add, asp = 1)
+            add = T
+        }
+    })
+}
---
+++ oRRRlova/Level/ui.R
@@ -0,0 +1,68 @@
+nameWidth = 2
+inputWidth = 10
+
+makeSlider = function(id, min, max, value, step = 1, label = NULL) {
+    sliderInput(
+        inputId = id,
+        label = label,
+        min = min,
+        max = max,
+        value = value,
+        step = step
+    )
+}
+
+makeCovMatrixRow = function(id, min, max, values) {
+    fluidRow(
+        column(nameWidth, h5("Ковар. матрица (разброс)")),
+        column(inputWidth,
+            fluidRow(
+                column(6, makeSlider(paste0(id, "11"), min, max, values[1])),
+                column(6, makeSlider(paste0(id, "12"), min, max, values[2])),
+                column(6, makeSlider(paste0(id, "21"), min, max, values[3])),
+                column(6, makeSlider(paste0(id, "22"), min, max, values[4]))
+            )
+        )
+    )
+}
+
+makeOutputRow = function(id) {
+    fluidRow(
+        column(nameWidth, NULL),
+        column(inputWidth, textOutput(id), style = "color: red; text-align: center")
+    )
+}
+
+makeByRow = function(byLimit) {
+    fluidRow(
+        column(nameWidth, "Шаг плотности"),
+        column(inputWidth,
+            fluidRow(
+                column(12, makeSlider("by", byLimit[1], byLimit[2], byLimit[3], byLimit[4]))
+            )
+        )
+    )
+}
+
+ui <- fluidPage(
+    titlePanel("Линии уровня"),
+
+    sidebarLayout(
+    
+        sidebarPanel(
+
+            makeCovMatrixRow("cov", 0, 10, c(1, 0, 0, 1)),
+            makeOutputRow("covMessage"),
+            makeByRow(c(0.001, 0.01, 0.005, 0.001))
+
+        ),
+
+        mainPanel(
+
+            plotOutput(outputId = "plot", height = "600px")
+
+        )
+    
+    )
+ 
+)
